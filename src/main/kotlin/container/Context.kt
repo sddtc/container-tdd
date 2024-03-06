67e403193811b55bcc7895fa3d678b000d872dfe
sddtc <changhbaga@gmail.com>
2024-03-06 23:59:14 +0800

    Component - bind type to a specific instance

--- src/main/kotlin/container/Context.kt
+++ src/main/kotlin/container/Context.kt
@@ -1,4 +1,15 @@
 package container
 
+import kotlin.reflect.KClass
+
 class Context {
+  private val components = HashMap<KClass<*>, Any>()
+
+  fun <Component : Any> get(type: KClass<Component>): Component {
+    return components[type] as Component
+  }
+
+  fun <Component : Any> bind(type: KClass<Component>, instance: Component) {
+    components[type] = instance
+  }
 }
--- src/test/kotlin/container/ContainerTest.kt
+++ src/test/kotlin/container/ContainerTest.kt
@@ -1,11 +1,20 @@
 package container
 
+import org.junit.jupiter.api.Assertions.assertSame
 import org.junit.jupiter.api.Test
 
 class ContainerTest {
-//  @Test
-//  fun `bind type to a specific instance`() {
-//  }
+  interface Component
+
+  @Test
+  fun `bind type to a specific instance`() {
+    val context = Context()
+    val instance = object : Component {}
+
+    context.bind(Component::class, instance)
+
+    assertSame(instance, context.get(Component::class))
+  }
 //
 //  @Test
 //  fun `bind type to a implementation class`() {
